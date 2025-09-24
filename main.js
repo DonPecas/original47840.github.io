20a444b81174fc7ffd0cf6e4a12368f7a048b9f5
DonPecas <isaiaselorca@gmail.com>
2025-09-24 08:57:41 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function mostrarFAQ() {
+    const faq = document.getElementById("faq-content");
+    faq.style.display = (faq.style.display === "none" || faq.style.display === "") ? "block" : "none";
+  }
+
+
+  
